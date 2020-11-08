0a5f2ea92a7ddbcadfde293583ae256cfc2517e1
Jinyang <jinyang@outlook.sg>
2020-11-08 19:01:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 //-- Prolog Session
 import subway_program from './subway-prolog.js'
 const session = pl.create()
-session.consult('./subway-prolog.pl' {
-    success: function(){},
-    error: function(err) {console.log(err)}
-})
+
+session.consult("./subway-prolog.pl" {
+    success: function() { /* Program loaded correctly */ },
+    error: function(err) { /* Error parsing program */ }
+});
 
 //-- Constants and Variables
 const user_avatar = 'https://image.flaticon.com/icons/svg/1400/1400241.svg'
